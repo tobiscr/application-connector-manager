608a73cfc1693f154b3b6b95d0a1ad2d4f4181f7
Przemysław Golicz <przemyslaw.golicz@sap.com>
2023-08-22 14:25:02 +0200

    Add RBACs to manage custom resource definitions

--- controllers/application_controller_rbac.go
+++ controllers/application_controller_rbac.go
@@ -19,3 +19,4 @@ package controllers
 //+kubebuilder:rbac:groups=autoscaling,resources=horizontalpodautoscalers,verbs=list;get;create;update;patch;delete;watch
 //+kubebuilder:rbac:groups=networking.istio.io,resources=gateways,verbs=list;get;create;update;patch;delete;watch
 //+kubebuilder:rbac:groups=networking.istio.io,resources=virtualservices,verbs=list;get;create;update;patch;delete;watch
+//+kubebuilder:rbac:groups=apiextensions.k8s.io,resources=customresourcedefinitions,verbs=list;watch;delete;create;patch;get
