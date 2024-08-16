f7940ed2474775e1da9d635d76633a516c17d6f5
Tobias Schuhmacher <t.schuhmacher@sap.com>
2024-08-16 14:28:05 +0200

    Add cobra and viper lib for opts parsing

--- components/central-application-gateway/cmd/applicationgateway/applicationproxy.go
+++ components/central-application-gateway/cmd/applicationgateway/applicationproxy.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"net"
 	"net/http"
 	"os"
 	"os/signal"
 	"strconv"
+	"strings"
 	"syscall"
 	"time"
 
@@ -24,6 +27,8 @@ import (
 	"github.com/kyma-project/kyma/components/central-application-gateway/pkg/client/clientset/versioned"
 	"github.com/kyma-project/kyma/components/central-application-gateway/pkg/httptools"
 	"github.com/oklog/run"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"go.uber.org/zap"
 	"k8s.io/client-go/kubernetes"
 	restclient "k8s.io/client-go/rest"
@@ -38,14 +43,84 @@ func main() {
 	setupLogger := zap.Must(zap.NewProduction())
 	defer func(setupLogger *zap.Logger) {
 		err := setupLogger.Sync()
-		if err != nil {
-			panic(err)
+		if err != nil && !errors.Is(err, syscall.ENOTTY) {
+			panic(fmt.Sprintf("Failed to synchronize logger: %v", err))
 		}
 	}(setupLogger)
 
-	setupLogger.Info("Starting Application Gateway")
+	initViper(setupLogger)
 
-	options := parseArgs(setupLogger)
+	//init command
+	opts := &options{}
+	var rootCmd = &cobra.Command{
+		Use:   "acm",
+		Short: "Root short description",
+		Long:  "Root long description",
+		Run: func(cmd *cobra.Command, args []string) {
+			runCmd(setupLogger, opts)
+		},
+	}
+
+	//parse args
+	parseArgs(rootCmd, opts, setupLogger)
+
+	if err := rootCmd.Execute(); err != nil {
+		setupLogger.Error("Failed to execute command: %v", zap.Error(err))
+	}
+}
+
+func initViper(setupLogger *zap.Logger) {
+	viper.SetEnvPrefix("ACM_GATEWAY")
+	viper.AutomaticEnv()
+
+	configFile := viper.GetString("config")
+	if len(configFile) > 0 {
+		err := viper.ReadInConfig()
+		if err != nil {
+			if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+				setupLogger.Error("Config file '%s' not found. Check the path provided in env-var '%s'",
+					zap.String("configFile", configFile),
+					zap.String("envVar", strings.ToUpper(fmt.Sprintf("%s_%s", viper.GetEnvPrefix(), "config"))))
+			} else {
+				setupLogger.Panic("Failed to parse config file: %v", zap.Error(err))
+			}
+		}
+	}
+}
+
+func parseArgs(rootCmd *cobra.Command, opts *options, setupLogger *zap.Logger) {
+	rootCmd.Flags().StringVar(&opts.apiServerURL, "apiServerURL", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
+	rootCmd.Flags().StringVar(&opts.applicationSecretsNamespace, "applicationSecretsNamespace", "kyma-system", "Namespace where Application secrets used by the Application Gateway exist")
+	rootCmd.Flags().IntVar(&opts.externalAPIPort, "externalAPIPort", 8081, "Port that exposes the API which allows checking the component status and exposes log configuration")
+	rootCmd.Flags().StringVar(&opts.kubeConfig, "kubeConfig", "", "Path to a kubeconfig. Only required if out-of-cluster")
+	opts.logLevel = zap.LevelFlag("logLevel", zap.InfoLevel, "Log level: panic | fatal | error | warn | info | debug. Can't be lower than info")
+	rootCmd.Flags().IntVar(&opts.proxyCacheTTL, "proxyCacheTTL", 120, "TTL, in seconds, for proxy cache of Remote API information")
+	rootCmd.Flags().IntVar(&opts.proxyPort, "proxyPort", 8080, "Port that acts as a proxy for the calls from services and Functions to an external solution in the default standalone mode or Compass bundles with a single API definition")
+	rootCmd.Flags().IntVar(&opts.proxyPortCompass, "proxyPortCompass", 8082, "Port that acts as a proxy for the calls from services and Functions to an external solution in the Compass mode")
+	rootCmd.Flags().IntVar(&opts.proxyTimeout, "proxyTimeout", 10, "Timeout for requests sent through the proxy, expressed in seconds")
+	rootCmd.Flags().IntVar(&opts.requestTimeout, "requestTimeout", 10, "Timeout for requests sent through Central Application Gateway, expressed in seconds")
+
+	opts.logArgs(setupLogger)
+}
+
+func (o options) logArgs(log *zap.Logger) {
+	log.Info("Parsed flags",
+		zap.String("-apiServerURL", o.apiServerURL),
+		zap.String("-applicationSecretsNamespace", o.applicationSecretsNamespace),
+		zap.Int("-externalAPIPort", o.externalAPIPort),
+		zap.String("-kubeConfig", o.kubeConfig),
+		zap.String("-logLevel", o.logLevel.String()),
+		zap.Int("-proxyCacheTTL", o.proxyCacheTTL),
+		zap.Int("-proxyPort", o.proxyPort),
+		zap.Int("-proxyPortCompass", o.proxyPortCompass),
+		zap.Int("-proxyTimeout", o.proxyTimeout),
+		zap.Int("-requestTimeout", o.requestTimeout),
+	)
+}
+
+func runCmd(setupLogger *zap.Logger, options *options) {
+
+	setupLogger.Info("Starting Application Gateway")
 
 	logCfg := zap.NewProductionConfig()
 	logCfg.Level.SetLevel(*options.logLevel)
@@ -82,8 +157,8 @@ func main() {
 		log.Fatal("Unable to create ServiceDefinitionService:'", zap.Error(err))
 	}
 
-	internalHandler := newInternalHandler(serviceDefinitionService, options)
-	internalHandlerForCompass := newInternalHandlerForCompass(serviceDefinitionService, options)
+	internalHandler := newInternalHandler(serviceDefinitionService, *options)
+	internalHandlerForCompass := newInternalHandlerForCompass(serviceDefinitionService, *options)
 	externalHandler := externalapi.NewHandler(logCfg.Level)
 
 	internalHandler = httptools.RequestLogger("Internal handler: ", internalHandler)
