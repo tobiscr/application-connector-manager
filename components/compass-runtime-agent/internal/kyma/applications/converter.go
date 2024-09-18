e224ec27363c89808d33b35e9738958a9e72ee8e
Przemysław Golicz <przemyslaw.golicz@sap.com>
2024-09-18 11:25:29 +0200

    Start working on application name normalisation in compass runtime agent

--- components/compass-runtime-agent/internal/kyma/applications/converter.go
+++ components/compass-runtime-agent/internal/kyma/applications/converter.go
@@ -58,7 +58,7 @@ func (c converter) Do(application model.Application) v1alpha1.Application {
 			APIVersion: "applicationconnector.kyma-project.io/v1alpha1",
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name:   application.Name,
+			Name:   application.Name, // here must normalize name
 			Labels: map[string]string{managedByLabelKey: managedByLabelValue},
 		},
 		Spec: v1alpha1.ApplicationSpec{
